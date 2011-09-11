a014806c5d9811f19cc35044eff7c81ee3339b58
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2011-09-11 11:34:58 -0300

    create javascript caller

--- rajesh/__init__.py
+++ rajesh/__init__.py
@@ -3,9 +3,28 @@ from twisted.web.static import File
 from txWebSocket.websocket import WebSocketHandler, WebSocketSite
 
 
+class JavaScript(object):
+
+    def __init__(self, write):
+        self._write = write
+        self._command = ""
+
+    def __getattr__(self, name):
+        if self._command:
+            self._command += ".%s" % name
+        else:
+            self._command = name
+        return self
+
+    def __call__(self, *args):
+        self._write("%s(%s)" % (self._command, ", ".join(map(repr, args))))
+        self._command = ""
+
+
 class Application(WebSocketHandler):
 
     def connectionMade(self):
+        self.js = JavaScript(self.transport.write)
         self.begin()
 
 
