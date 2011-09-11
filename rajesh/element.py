28b71545cbaa32b300d5df486cb16c3d946c87d4
Ângelo Otávio Nuffer Nunes <angelonuffer@gmail.com>
2011-09-11 13:25:30 -0300

    create html element

---
+++ rajesh/element.py
@@ -0,0 +1,18 @@
+class Element(object):
+
+    def __init__(self, name, **kwargs):
+        self.name = name
+        self.parameters = kwargs
+        self.text = ""
+
+    def __repr__(self):
+        name = self.name
+        parameters = " ".join(["%s=\"%s\"" % item for item in self.parameters.items()])
+        text = self.text
+        return "<%(name)s %(parameters)s>%(text)s</%(name)s>" % locals()
+
+
+class Button(Element):
+
+    def __init__(self, **kwargs):
+        super(Button, self).__init__("button", **kwargs)
